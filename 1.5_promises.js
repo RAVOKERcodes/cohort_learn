27a2337e61625457198e34b65c30acb50ef01418
RAVOKERcodes <shridhardhruv123@gmail.com>
2024-06-16 18:38:59 +0530

    test

---
+++ 1.5_promises.js
@@ -0,0 +1,33 @@
+
+
+
+//=============================================================//
+
+// function ravo(){
+
+//     let p= new Promise(function(resolve){      // dummy async funtrion that almost immidiatly rexsolves
+//         resolve("hope");
+//     });
+//     return p
+
+// }
+
+// ravo().then(function(data){             // actually logging the data what function above resolves     console.log(data);
+//   console.log(data) ;
+//}) 
+
+//=============================================================//
+
+// let p = new Promise(function(rrrr){
+
+//     rrrr("has")
+
+// })
+// p.then(()=>{                            //arrow function // ()=>   ---   function()
+//     console.log(p);
+// }) 
+
+// // .then is called when ever the async funct5ion is resolves// rrrr = resolve
+
+//=============================================================//
+
